f80a14f5f45db5d77a1b1234a7115246f746a761
Stuart Thomson <stu.thom.96@gmail.com>
2018-07-22 09:51:02 +1200

    Widgets define an icon/title/default data

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
+import ReactSVG from 'react-svg';
+import defaultIcon from '../../assets/sharp-help-24px.svg';
 // import './index.css';
 import { IState, IProps } from '../../types';
 import { createInactivityTimeout } from '../../util';
 
 const STATE_SAVE_TIMEOUT = 300;
 
-export default class Widget<State extends IState> extends React.Component<IProps<State>, State> {
+export default class Widget<State extends IState = IState> extends React.Component<IProps<State>, State> {
+  static title = 'Unknown';
   private saveHandler: (state: IState) => void;
+
+  static renderIcon() {
+    return (
+      <ReactSVG path={defaultIcon} className="icon" />
+    );
+  }
+
+  static getDefaultData() {
+    return {};
+  }
 
   constructor(props: IProps<State>) {
     super(props);
