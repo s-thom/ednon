32fdcdc3ef7795314014307c5fa2ff645dbf6c3a
Stuart Thomson <stu.thom.96@gmail.com>
2018-07-22 09:04:46 +1200

    Add inactivity timeout to state saving

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,22 +1,28 @@
 import * as React from 'react';
-import autobind from 'autobind-decorator';
-import IntervalRenderer from '../IntervalRenderer';
-import TimerDisplay from '../TimerDisplay';
 // import './index.css';
 import { IState, IProps } from '../../types';
+import { createInactivityTimeout } from '../../util';
 
-class Widget<State extends IState> extends React.Component<IProps<State>, State> {
+const STATE_SAVE_TIMEOUT = 300;
+
+export default class Widget<State extends IState> extends React.Component<IProps<State>, State> {
+  private saveHandler: (state: IState) => void;
+
   constructor(props: IProps<State>) {
     super(props);
 
     this.state = props.data;
+
+    this.saveHandler = createInactivityTimeout((s) => this.saveState(s), STATE_SAVE_TIMEOUT);
   }
 
   setState(state) {
     super.setState(state);
 
+    this.saveHandler(state);
+  }
+
+  saveState(state) {
     this.props.storage.saveWidgetState(this.props.id, state);
   }
 }
-
-export default Widget;
